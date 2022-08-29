50dd4901f16fb212fadc0eb02b585f6493e7e93f
Dr. AInme <109267676+HappyOracle@users.noreply.github.com>
2022-08-29 18:03:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,61 +3,82 @@ import pyodbc
 
 
 connGG = pyodbc.connect('Driver={SQL SERVER};'
-                        'Server=123;'
+                        'Server=10.110.10.29;'
                         'Database=DB-TG;'
                         'UID=sa;'
-                        'PWD=;')
+                        'PWD=Aa123456;'
+                        'Trusted_Connection=yes;''autocommit=True')
 
 
 cursor = connGG.cursor()
 
-#connGG.setdecoding(pyodbc.SQL_CHAR, encoding='utf-16')
-#connGG.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-16')
-#connGG.setencoding(encoding='utf-16')
-
-SQLQuery = ("""
-                SELECT TG_message_id, TG_message_text, TG_message_is_deleted
-                FROM [DB-TG].dbo.[5276890896] 
+useranon = "SELECT * FROM useranon"
+cursor.execute(useranon)
 
 
-            """) #5276890896
+SQLinsert = '''INSERT INTO useranon (Int_ID, TG_message_id, TG_message_text, TG_message_is_deleted, TG_message_is_forwarded, TG_message_is_file, TG_message_file_content)
+                VALUES (?,?,?,?,?,?,?);'''
 
-cursor.execute(SQLQuery)
-results = cursor.fetchall()
 
-cursor.execute('''INSERT INTO (TG_message_id, TG_message_text, TG_message_is_deleted)
-                           VALUES 
-                           404, 'тест ыыыытест', 1 ''')
+results = cursor.fetchall() #[0] fetchall
+for row in results:
+    Int_ID = row[0]# row(int[0])
+    TG_message_id = row[1]
+    TG_message_text = row[2]
+    TG_message_is_deleted = row[3]
+    TG_message_is_forwarded = row[4]
+    TG_message_is_file = row[5]
+    TG_message_file_content = row[6]
 
-SQLCommand = ("INSERT INTO tbl_employee(id,firstName, LastName, EmployeeId) VALUES (?,?,?,?)")    
-Values = [Id,firstname,LastName,RegisterNo]   
+    values = (row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+#   print(Int_ID, TG_message_id, TG_message_text, TG_message_is_deleted, TG_message_is_forwarded, TG_message_is_file, TG_message_file_content)
+    print(values)
+#cursor.execute(SQLinsert, results()) # не работает
+cursor.execute("SELECT * from useranon") #работает, но что-то не так...
+
 connGG.commit()
 
+#cursor.execute('SELECT * FROM useranon')
+
+#def read(connGG):
+#    print("Read")
+#    cursor = connGG.cursor
+#    cursor.execute('SECELT * from dummy')
+#    for row in cursor:
+#        print(f'row = {row}')
+#    print()
 
 
-#for row in cursor:
-#    print(row.TG_message_id, row.TG_message_text, row.TG_message_is_deleted) #worked
+#def create(connGG):
+#    print("Create")
+#    cursor = connGG.cursor
+#    cursor.execute(
+#        'INSERT INTO dummy(a,b) values (?,?);',
+#        (3232, 'catzozo')
+#    )
+#    connGG.commit()
+#    read(connGG)
 
 
+#def update(connGG):
+#    print("update")
+#    cursor = connGG.cursor
+#    cursor.execute(
+#        'UPDATE dummy set b = ? where a = ?;',
+#        ('dogzozo', 3232)
+#    )
+#    connGG.commit()
+#    read(connGG)
 
+#def delete(connGG):
+#    print("Delete")
+#    cursor = connGG.cursor
+#    cursor.execute(
+#        'DELETE FROM dummy WHERE a > 5'
+#    )
+#    connGG.commit()
+#    read(connGG)
 
-
-#rows = cursor.fetchall()
-#for row in rows:
-#    print(row.TG_message_id, row.TG_message_text, row.TG_message_is_deleted) #all tables
-
-for row in results:
-    TG_message_id = row[0]
-    TG_message_text = row[1]
-    TG_message_is_deleted = row[2]
-
-
-#print(TG_message_id)
-#print(TG_message_text)
-#print(TG_message_is_deleted)
-
-
-#print(results)
 
 #read(connGG)
 #create(connGG)
@@ -65,23 +86,3 @@ for row in results:
 #delete(connGG)
 
 connGG.close()
-
-
-#
-#def read(connGG):
-#    print("Read")
-#    cursor = connGG.cursor
-#    cursor.execute('SECELT * from [DB-TG].dbo.[5276890896]')
-#    for row in cursor:
-#        print(f'row = {row}')
-#    print()
-#
-#def create(connGG):
-#    print("Create")
-#    cursor = connGG.cursor
-#    cursor.execute(
-#        'INSERT INTO [DB-TG].dbo.[5276890896](a,b) values (?,?);',
-#        (3232, 'catzozo')
-#   )
-#    connGG.commit()
-#    read(connGG)
